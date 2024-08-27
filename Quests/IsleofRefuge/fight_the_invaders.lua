593322e9681dab2c9837ca61687f9c1bd26c1e8c
Emagi <image.emagi@gmail.com>
2024-08-27 12:29:47 -0400

    Revert changes that should not have been put in main

--- Quests/IsleofRefuge/fight_the_invaders.lua
+++ Quests/IsleofRefuge/fight_the_invaders.lua
@@ -4,17 +4,16 @@
     Script Date    : 2024.07.11 04:07:42
     Script Purpose : 
 
-        Zone       : Thunderdome
-        Quest Giver: Bubbles
+        Zone       : IsleofRefuge
+        Quest Giver: Nathinia Sparklebright or Geredo
         Preceded by: None
         Followed by: 
 --]]
 require "SpawnScripts/Generic/DialogModule"
 
 function Init(Quest)
-    AddQuestStepKill(Quest, 1, "I must kill five Gruttooth Invaders here in the Thunderdome", 5, 100, "I need to kill Gruttooth Invaders", 372, 3250063)
-    AddQuestStepCompleteAction(Quest, 1, "Step1Complete")
-    UpdateQuestZone(Quest, "Thunderdome")
+    AddQuestStep(Quest, 1, "I must kill five Gruttooth Invaders on the Isle of Refuge", 5, 100, "I need to kill Gruttooth Invaders", 3250063)
+    AddQuestStepCompleteAction(Quest, 1, "QuestComplete")
 end
 
 function Accepted(Quest, QuestGiver, Player)
@@ -31,16 +30,10 @@ function Declined(Quest, QuestGiver, Player)
 
 end
 
-function Step1Complete(Quest, QuestGiver, Player)
+function QuestComplete(Quest, QuestGiver, Player)
     UpdateQuestStepDescription(Quest, 1, "I've killed 5 Gruttooth Invaders")
     UpdateQuestTaskGroupDescription(Quest, 1, "All the Gruttooth Invaders are dead")
-    UpdateQuestZone(Quest, "Thunderdome")
-    AddQuestStepChat(Quest, 2, "I should speak with Bubbles again.", 1, "All the goblins are dead.", 372, 8500000)
-    AddQuestStepCompleteAction(Quest, 2, "QuestComplete")
-end
-
-function QuestComplete(Quest, QuestGiver, Player)
-    UpdateQuestDescription(Quest, "Bubbles says my efforts will bring a measure of relief. He gave me some coin for my time")
+    UpdateQuestDescription(Quest, "Nathinia says my efforts will bring a measure of relief. She gave me some coin for my time")
     
     GiveQuestReward(Quest, Player)
 
