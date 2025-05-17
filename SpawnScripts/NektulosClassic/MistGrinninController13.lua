1479ddc136345d8a3a1021ba04cce406bcd52342
Emagi <image.emagi@gmail.com>
2025-05-17 00:00:05 +0000

    content updates

---
+++ SpawnScripts/NektulosClassic/MistGrinninController13.lua
@@ -0,0 +1,34 @@
+--[[
+    Script Name    : SpawnScripts/NektulosClassic/MistGrinninController13.lua
+    Script Author  : lordpazuzu
+    Script Date    : 2025.05.16 07:05:47
+    Script Purpose : 
+                   : 
+--]]
+
+require "SpawnScripts/Generic/NPCModule"
+
+function spawn(NPC)
+    local zone = GetZone(NPC)
+    ControlObject = GetSpawnLocationID(NPC)
+    GroupID = 1058143
+    LevelMin = 24
+    LevelMax = 25
+
+    SpawnGroupByID(zone, GroupID, MakeRandomInt(LevelMin,LevelMax))
+    AddTimer(NPC, 6000, "SpawnCheck")
+end
+
+function SpawnCheck(NPC)
+    local zone = GetZone(NPC)
+    
+    if  IsSpawnGroupAlive(zone, GroupID) == true then
+        AddTimer(NPC, 6000, "SpawnCheck")
+    else
+        Despawn(GetSpawnByLocationID(zone, ControlObject))
+    end
+end
+
+function respawn(NPC)
+	spawn(NPC)
+end
