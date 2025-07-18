99c1dfb67dafb9985ad3b8cf5fe8b6399272c498
Emagi <image.emagi@gmail.com>
2025-07-18 00:00:22 +0000

    content updates

---
+++ SpawnScripts/Blackburrow/MineController9.lua
@@ -0,0 +1,42 @@
+--[[
+    Script Name    : SpawnScripts/Blackburrow/MineController9.lua
+    Script Author  : lordpazuzu
+    Script Date    : 2025.07.17 06:07:36
+    Script Purpose : 
+                   : 
+--]]
+
+require "SpawnScripts/Generic/NPCModule"
+
+function spawn(NPC)
+    local zone = GetZone(NPC)
+    ControlObject = GetSpawnLocationID(NPC) -- Loc ID for spawn object
+    LevelMin = 13 -- Minium level for encounter to spawn at
+    LevelMax = 14 -- Maximum level for encounter to spawn at
+    Group1 = 1058261 -- Group ID
+    Group2 = 1058262 -- Group ID
+    SpawnVariant = MakeRandomInt(1,2)
+    
+    if SpawnVariant == 1 then 
+        GroupID = Group1
+    elseif SpawnVariant == 2 then
+        GroupID = Group2
+    end
+    
+    SpawnGroupByID(zone, GroupID, MakeRandomInt(LevelMin,LevelMax))
+    AddTimer(NPC, 6000, "SpawnCheck")
+end
+
+function SpawnCheck(NPC)
+    local zone = GetZone(NPC)
+    
+    if  IsSpawnGroupAlive(zone, GroupID) == true then
+        AddTimer(NPC, 6000, "SpawnCheck")
+    else
+        Despawn(GetSpawnByLocationID(zone, ControlObject))
+    end
+end
+
+function respawn(NPC)
+    spawn(NPC)
+end
