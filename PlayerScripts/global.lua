0dacdbd375af0b383ddd3b14f89c553fefc04741
Emagi <image.emagi@gmail.com>
2025-08-28 00:00:05 +0000

    content updates

--- PlayerScripts/global.lua
+++ PlayerScripts/global.lua
@@ -6,39 +6,117 @@
                    : 
 --]]
 local specialZones = {
-  [26]  = true,  -- FarJourneyQeynos
-  [27]  = true,  -- FarJourneyFreeport
-  [253] = true,  -- QueensColony
-  [254] = true,  -- SourceEvil
-  [255] = true,  -- MizansCellar
-  [278] = true,  -- OutpostOverlord
-  [314] = true,  -- IsleRefuge2
-  [324] = true,  -- GrimgashsLair
-  [325] = true,  -- IsleRefuge1
-  [348] = true,  -- CircleElders
-  [450] = true,  -- SmallHideout
-  [665] = true,  -- DaratharsFlight
-  [770] = true,  -- GrozmagsTrial
-  [775] = true,  -- exp12_dun_ancientruins
-  [804] = true,  -- home_tutorial
-  [828] = true,  -- ClassicFarJourney
-  [838] = true,  -- GaranelRestingPlace
-  [846] = true,  -- LCP
+    [26] = true, -- FarJourneyQeynos
+    [27] = true, -- FarJourneyFreeport
+    [253] = true, -- QueensColony
+    [254] = true, -- SourceEvil
+    [255] = true, -- MizansCellar
+    [278] = true, -- OutpostOverlord
+    [314] = true, -- IsleRefuge2
+    [324] = true, -- GrimgashsLair
+    [325] = true, -- IsleRefuge1
+    [348] = true, -- CircleElders
+    [450] = true, -- SmallHideout
+    [665] = true, -- DaratharsFlight
+    [770] = true, -- GrozmagsTrial
+    [775] = true, -- exp12_dun_ancientruins
+    [804] = true, -- home_tutorial
+    [828] = true, -- ClassicFarJourney
+    [838] = true, -- GaranelRestingPlace
+    [846] = true -- LCP
+}
+
+local archetypeClasses = {
+    [1] = true, -- fighter
+    [11] = true, -- priest
+    [21] = true, -- mage
+    [31] = true -- scout
+}
+
+local lowerClasses = {
+    [2] = true, -- warrior
+    [5] = true, -- brawler
+    [6] = true, -- monk
+    [8] = true, -- crusader
+    [12] = true, -- cleric
+    [15] = true, -- druid
+    [18] = true, -- shaman
+    [22] = true, -- sorcerer
+    [23] = true, -- wizard
+    [25] = true, -- enchanter
+    [28] = true, -- summoner
+    [32] = true, -- rogue
+    [35] = true, -- bard
+    [38] = true, -- predator
+    [41] = true, -- animalist
+    [43] = true -- shaper
 }
 
 function on_level_up(Zone, Player, OldLevel, NewLevel, ExpEarned)
     local zoneID = GetZoneID(Zone)
-    if specialZones[zoneID] then
-        local currentXP = GetInfoStructUInt(Player, "xp")
-        local maxXP = tonumber(GetExpRequiredByLevel(7)) + tonumber(GetExpRequiredByLevel(7)) + tonumber(GetExpRequiredByLevel(7) * .2) - ExpEarned
-        if NewLevel > 6 or (OldLevel == 6 and currentXP > maxXP) then
-            if currentXP > maxXP then
-                SendPopUpMessage(Player, "You cannot gain anymore experience until you gain your next level.", 255, 255, 255)
-                SetInfoStructUInt(Player, "xp", maxXP)
-                SetCharSheetChanged(Player, true)
-            end
-        SendPopUpMessage(Player, "You must leave the Isle to gain levels.", 255, 255, 255)
-        return -1
-        end
+    local playerClass = GetClass(Player)
+    if archetypeClasses[playerClass] or lowerClasses[playerClass] then
+        try_class_update(Player, NewLevel)
     end
+    if GetClass(Player) == 0 then
+        return validate_level_xp(
+            Player,
+            OldLevel,
+            NewLevel,
+            ExpEarned,
+            3,
+            "You must select your archetype to gain levels."
+        )
+    elseif archetypeClasses[playerClass] then
+        return validate_level_xp(Player, OldLevel, NewLevel, ExpEarned, 9, "You must choose your class to gain levels.")
+    elseif lowerClasses[playerClass] then
+        return validate_level_xp(
+            Player,
+            OldLevel,
+            NewLevel,
+            ExpEarned,
+            19,
+            "You must choose your sub-class to gain levels."
+        )
+    end
+
+    -- check isle of refuge independently
+    if specialZones[zoneID] then
+        return validate_level_xp(Player, OldLevel, NewLevel, 6, "You must leave the Isle to gain levels.")
+    end
+end
+
+function try_class_update(Player, NewLevel)
+    local classSelect = GetInfoStructString(Player, "adventure_class_selection")
+    local classNum = tonumber(classSelect)
+    if classSelect ~= "" and classNum then
+        SetAdventureClass(Player, classNum)
+        SetInfoStructString(Player, "adventure_class_selection", "") -- clear out since we set the adventure class
+    end
+end
+
+function validate_level_xp(Player, OldLevel, NewLevel, ExpEarned, MaxLevel, Message)
+    local currentXP = GetInfoStructUInt(Player, "xp")
+    local MaxLvlPlus = MaxLevel + 1
+    local maxXP =
+        tonumber(GetExpRequiredByLevel(MaxLvlPlus)) + tonumber(GetExpRequiredByLevel(MaxLvlPlus)) +
+        tonumber(GetExpRequiredByLevel(MaxLvlPlus) * .2) -
+        ExpEarned
+    if NewLevel > MaxLevel or (OldLevel == MaxLevel and currentXP > maxXP) then
+        if currentXP > maxXP then
+            SendPopUpMessage(
+                Player,
+                "You cannot gain anymore experience until you gain your next level.",
+                255,
+                255,
+                255
+            )
+            SetInfoStructUInt(Player, "xp", maxXP)
+            SetCharSheetChanged(Player, true)
+        end
+        SendPopUpMessage(Player, Message, 255, 255, 255)
+        return -1
+    end
+
+    return 1
 end
