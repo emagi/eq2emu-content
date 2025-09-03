607505f9be9ab4fffe85a324352ac17d8d1a9084
Emagi <image.emagi@gmail.com>
2025-09-03 00:00:06 +0000

    content updates

---
+++ SpawnScripts/VerminsSnye_Classic/NookController4.lua
@@ -0,0 +1,47 @@
+--[[
+    Script Name    : SpawnScripts/VerminsSnye_Classic/NookController4.lua
+    Script Author  : lordpazuzu
+    Script Date    : 2025.09.02 08:09:14
+    Script Purpose : 
+                   : 
+--]]
+
+require "SpawnScripts/Generic/NPCModule"
+
+function spawn(NPC)
+    local zone = GetZone(NPC)
+    ControlObject = GetSpawnLocationID(NPC) -- Loc ID for spawn object
+    LevelMin = 11 -- Minium level for encounter to spawn at
+    LevelMax = 12 -- Maximum level for encounter to spawn at
+    Group1 = 1058410 -- Group ID
+    Group2 = 1058411 -- Group ID
+    Group3 = 1058412-- Group ID
+    SpawnVariant = MakeRandomInt(1,100)
+    
+    if SpawnVariant <= 47 then 
+        GroupID = Group1
+    elseif SpawnVariant > 47 and SpawnVariant < 95 then
+        GroupID = Group2
+    elseif SpawnVariant >= 95 then
+        GroupID = Group3
+        LevelMin = 20 -- Minium level for encounter to spawn at
+        LevelMax = 20 -- Maximum level for encounter to spawn at
+    end
+    
+    SpawnGroupByID(zone, GroupID, MakeRandomInt(LevelMin,LevelMax))
+    AddTimer(NPC, 6000, "SpawnCheck")
+end
+
+function SpawnCheck(NPC)
+    local zone = GetZone(NPC)
+    
+    if  IsSpawnGroupAlive(zone, GroupID) == true then
+        AddTimer(NPC, 6000, "SpawnCheck")
+    else
+        Despawn(GetSpawnByLocationID(zone, ControlObject))
+    end
+end
+
+function respawn(NPC)
+    spawn(NPC)
+end
